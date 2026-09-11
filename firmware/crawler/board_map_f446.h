4adea73c73b8c66e0b15d87d6a54228a29e717ef
lelelelelelele543-star <lelelelelelele543@gmail.com>
2026-09-11 10:53:22 +0300

    Rev.B WB10: move pressure to I2C and add sampled leak probe pins

--- firmware/crawler/board_map_f446.h
+++ firmware/crawler/board_map_f446.h
@@ -1,5 +1,5 @@
 #pragma once
-/* PX-1 crawler / NUCLEO-F446RE / Rev.B WB09 logical pin map */
+/* PX-1 crawler / NUCLEO-F446RE / Rev.B WB10 logical pin map */
 
 /*
  * Main tether RS-485
@@ -20,9 +20,18 @@
 #define PX1_CAMERA_TX "PC12/UART5_TX"
 #define PX1_CAMERA_RX "PD2/UART5_RX"
 
-/* Ready current/power sensors */
+/*
+ * I2C1 service bus
+ * Direct: LEFT/RIGHT INA226-class current sensors + TCA9548A mux.
+ * TCA9548A ch0/ch1/ch2: P0/P1/P2 LPS28 pressure sensors.
+ */
 #define PX1_I2C_SCL "PB8/I2C1_SCL"
 #define PX1_I2C_SDA "PB9/I2C1_SDA"
+#define PX1_PRESSURE_MUX_ADDR 0x70
+#define PX1_PRESSURE_P0_CH 0
+#define PX1_PRESSURE_P1_CH 1
+#define PX1_PRESSURE_P2_CH 2
+#define PX1_LPS28_ADDR 0x5C
 
 /* Traction */
 #define PX1_TRACTION_L_PWM "PA8/TIM1_CH1"
@@ -30,8 +39,9 @@
 #define PX1_WHEEL_L_ADC "PA0/ADC1_IN0"
 #define PX1_WHEEL_R_ADC "PA1/ADC1_IN1"
 
-/* Sensors */
-#define PX1_PRESSURE_ADC "PC0/ADC1_IN10"
+/* Sensors / telemetry */
+#define PX1_LEAK_ADC "PC0/ADC1_IN10"
+#define PX1_LEAK_EXCITE "PB15"
 #define PX1_BUS_ADC "PC1/ADC1_IN11"
 #define PX1_TEMP_ADC "PC2/ADC1_IN12"
 
@@ -44,4 +54,4 @@
 
 /* Safety */
 #define PX1_ESTOP "PB13"
-#define PX1_LEAK "PB14"
+#define PX1_LEAK_DIGITAL_RESERVE "PB14"
